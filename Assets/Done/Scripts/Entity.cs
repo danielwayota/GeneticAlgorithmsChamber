ea3bbf5d9ba25999419205d6c6ae1d608fb9b990
Daniel Wayota <danielglez1990@gmail.com>
2019-05-28 16:27:04 +0100

    WIP: Population and generations over time. Need to be tested

--- Assets/Done/Scripts/Entity.cs
+++ Assets/Done/Scripts/Entity.cs
@@ -2,18 +2,86 @@
 
 namespace Done
 {
+    [RequireComponent(typeof(Rigidbody))]
     public class Entity : MonoBehaviour
     {
-        // Start is called before the first frame update
+        public Vector3[] genes { get; set; }
+        public float forcePower = 32;
+
+        private int geneIndex;
+        private int geneCount = 100;
+
+        private float time = 0;
+        private float timeOut = 0.5f;
+
+        private EntityState state;
+
+        private Rigidbody body;
+
+        // ========================================================
         void Start()
         {
-            
+
+            if (this.genes == null)
+            {
+                Vector3[] startGenes = new Vector3[this.geneCount];
+
+                for (int i = 0; i < this.geneCount; i++)
+                {
+                    startGenes[i] = (new Vector3(
+                        Random.Range(-1f, 1f),
+                        Random.Range(-1f, 1f),
+                        Random.Range(-1f, 1f)
+                    )).normalized;
+                }
+
+                this.genes = startGenes;
+            }
+
+            this.body = GetComponent<Rigidbody>();
+            this.geneIndex = 0;
+            this.state = EntityState.MOVING;
+            this.body.velocity = Vector3.zero;
         }
 
-        // Update is called once per frame
+        // ========================================================
+        private void FixedUpdate()
+        {
+            if (this.state == EntityState.MOVING)
+            {
+                Vector3 forceDirection = this.genes[this.geneIndex];
+
+                this.body.AddForce(forceDirection * this.forcePower);
+            }
+        }
+
+        // ========================================================
         void Update()
         {
-            
+            this.time += Time.deltaTime;
+            if (this.time > this.timeOut)
+            {
+                this.time -= this.timeOut;
+
+                switch(this.state)
+                {
+                    case EntityState.MOVING:
+                        this.state = EntityState.IDLE;
+                        break;
+                    case EntityState.IDLE:
+                        if (this.geneIndex < (this.geneCount-1))
+                        {
+                            this.geneIndex++;
+                        }
+                        this.state = EntityState.MOVING;
+                        break;
+                }
+            }
         }
+    }
+
+    enum EntityState
+    {
+        MOVING, IDLE
     }
 }
--- Assets/Done/Scripts/Population.cs
+++ Assets/Done/Scripts/Population.cs
@@ -4,16 +4,144 @@ namespace Done
 {
     public class Population : MonoBehaviour
     {
-        // Start is called before the first frame update
+        public GameObject entityPrefab;
+
+        public int populationSize = 8;
+
+        public Transform startPoint;
+        public Transform targetPoint;
+
+        private Entity[] population;
+
+
+        private float time = 0;
+        private float timeOut = 10f;
+
+        // ========================================================
         void Start()
         {
-            
+            this.population = new Entity[this.populationSize];
+
+            for (int i = 0; i < this.populationSize; i++)
+            {
+                this.population[i] = this.CreateEntity();
+            }
         }
 
-        // Update is called once per frame
+        // ========================================================
         void Update()
         {
-            
+            this.time += Time.deltaTime;
+
+            if (this.time > this.timeOut)
+            {
+                this.time -= this.timeOut;
+
+                // Generate new population
+                Entity[] newPopulation = new Entity[this.populationSize];
+                Entity[] parents = new Entity[2];
+
+                for(int i = 0; i < this.populationSize; i++)
+                {
+                    Entity newborn = this.CreateEntity();
+                    int parentCount = 0;
+
+                    int safety = 0;
+                    while (parentCount < 2 && safety < 10000)
+                    {
+                        safety++;
+                        int index = Random.Range(0, this.populationSize);
+                        Entity candidate = this.population[index];
+                        float dice = Random.Range(0f, 1f);
+
+                        // Calculate fitness
+                        if (this.CalculateEntityFitness(candidate) > dice)
+                        {
+                            parents[parentCount] = candidate;
+                            parentCount++;
+                        }
+                    }
+
+                    if (parents.Length == 2)
+                    {
+                        // Crossover
+                        Vector3[] newGenes = this.Crossover(parents[0].genes, parents[1].genes);
+                        // Mutate
+                        newGenes = this.Mutate(newGenes, 0.05f);
+
+                        newborn.genes = newGenes;
+                    }
+                    else
+                    {
+                        Debug.LogError("No parents found. The Entity will be random.");
+                    }
+
+                    newPopulation[i] = newborn;
+                }
+
+                // Clear old population
+                foreach (Entity e in this.population)
+                {
+                    Destroy(e.gameObject);
+                }
+
+                // Hail to the new population!
+                this.population = newPopulation;
+            }
+        }
+
+        // ========================================================
+        Entity CreateEntity()
+        {
+            GameObject e = Instantiate(this.entityPrefab, this.startPoint.position, Quaternion.identity);
+
+            e.transform.SetParent(this.transform);
+
+            return e.GetComponent<Entity>();
+        }
+
+        // ========================================================
+        float CalculateEntityFitness(Entity e)
+        {
+            float maxDistance = Vector3.Distance(this.startPoint.position, this.targetPoint.position);
+            float entityDistance = Vector3.Distance(e.transform.position, this.targetPoint.position);
+
+            float percent = Mathf.Clamp01(entityDistance / maxDistance);
+
+            return 1 - percent;
+        }
+
+        // ========================================================
+        Vector3[] Mutate(Vector3[] e, float chance)
+        {
+            for (int i = 0; i < e.Length; i++)
+            {
+                float rnd = Random.Range(0f, 1f);
+                if (rnd < chance)
+                {
+                    e[i].Set(
+                        Random.Range(-1f, 1f),
+                        Random.Range(-1f, 1f),
+                        Random.Range(-1f, 1f)
+                    );
+                    e[i].Normalize();
+                }
+            }
+
+            return e;
+        }
+
+        // ========================================================
+        Vector3[] Crossover(Vector3[] a, Vector3[] b)
+        {
+            Vector3[] offspring = new Vector3[a.Length];
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                offspring[i] = i % 2 == 0 ? a[i] : b[i];
+            }
+
+            return offspring;
         }
     }
 }
